8fe9e0d8a77daeb467e540d6f4e06e9d1cdf73f3
Will Patterson <wpatter6@gmail.com>
2018-03-21 10:19:50 -0600

    Filtering order fix

--- IdleHeroesCalculator.Web/Scripts/app/vue-components.ts
+++ IdleHeroesCalculator.Web/Scripts/app/vue-components.ts
@@ -16,9 +16,9 @@ Vue.component("filter-item", {
 Vue.component("filter-items", {
     props: ["factions", "roles", "filter", "min-stars", "max-stars"],
     template: `<ul class="flist">
-                    <filter-item v-for="f in factions" :img="f.img" :name="f.name" :id="'f-' + f.id" :key="'faction-' + f.id" @click.native="filter"></filter-item>
-                    <li class="fseperator"/>
                     <filter-item v-for="r in roles" :img="r.img" :name="r.name" :id="'r-' + r.id" :key="'role-' + r.id" @click.native="filter"></filter-item>
                     <li class="fseperator"/>
-                    <filter-item v-for="s in maxStars-minStars" :name="s + minStars + ' star'" :id="'s-' + (s + minStars)" :key="'star-' + (s + minStars)" @click.native="filter"></filter-item>
+                    <filter-item v-for="f in factions" :img="f.img" :name="f.name" :id="'f-' + f.id" :key="'faction-' + f.id" @click.native="filter"></filter-item>
+                    <li class="fseperator"/>
+                    <filter-item v-for="s in maxStars-minStars" :name="maxStars - s + 1 + ' star'" :id="'s-' + (maxStars - s + 1)" :key="'star-' + (maxStars - s + 1)" @click.native="filter"></filter-item>
                 </ul>`});
